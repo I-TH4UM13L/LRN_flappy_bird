02df02f63114f6a134b2a6075acee4b5cc03432b
CrimsonPanda <45400656+udaravidunuwan@users.noreply.github.com>
2025-07-01 09:30:19 +0530

    Gameover mechanic Initialization

--- scene/Actor/player.gd
+++ scene/Actor/player.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+signal gameover
+
 @onready var anim_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
 
 const JUMP_velocity = -200.0
+const rot_speed = 300.0
 
 var velocity: Vector2 = Vector2.ZERO
 
@@ -16,7 +19,17 @@ func randon_bird() -> void:
 	
 func _process(delta):
 	velocity.y += gravity * delta
+	if rotation_degrees <= 45.0 and velocity.y > 0:
+		rotation_degrees += rot_speed * delta
+	else:
+		if rotation_degrees >= -45.0:
+			rotation_degrees -= rot_speed * delta  
 	if Input.is_action_just_pressed("flap"):
 		velocity.y = JUMP_velocity
 		anim_sprite.play()
 	position += velocity * delta
+
+
+func _on_body_entered(body):
+	anim_sprite.stop()
+	gameover.emit()
